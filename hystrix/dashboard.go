cddc8f5df4d5fd0c5b94e5de26161b90d3fb6dec
linimbus <linimbus@126.com>
2017-11-10 23:08:48 +0800

    update

--- hystrix/dashboard.go
+++ hystrix/dashboard.go
@@ -1,1 +1,23 @@
 package hystrix
+
+import (
+	"fmt"
+	"time"
+)
+
+func init() {
+	go showinfo()
+}
+
+func showinfo() {
+
+	fmt.Println("dashboard task start...")
+
+	for {
+
+		time.Sleep(1 * time.Second)
+		for name, cir := range circuit {
+			fmt.Println("cmd : ", name, " status : ", cir.status)
+		}
+	}
+}
